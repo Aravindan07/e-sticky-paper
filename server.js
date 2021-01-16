f02e1a02ca5f8337fd46cade73c7a5d3775473c0
Aravindan07 <50622300+Aravindan07@users.noreply.github.com>
2021-01-16 22:34:34 +0530

    add: added service worker

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const newUsers = require("./routes/api/users");
 const loginUser = require("./routes/api/login");
 const Goals = require("./routes/api/goals");
 const Notes = require("./routes/api/notes");
+const webPush = require("web-push");
 
 app.use(morgan("dev"));
 
@@ -26,7 +27,33 @@ mongoose
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log(err));
 
+const vapidKeys = webPush.generateVAPIDKeys();
+// const publicVapidKey =
+//   "BH2v8FAHYiT3n3l3qAQGo_FvJgytwDK2e1d9NJXyReF0TebIyii9prcZXIHHvngLvn6p-Efqc3MOOM6lh3_CQV0";
+// const privateVapidKey = "vdBFzfheha8rwQzOZ_ftSvMis4eGci8raSylt1J0Vss";
+
+webPush.setVapidDetails(
+  "mailto:aravindanravi33@gmail.com",
+  vapidKeys.publicKey,
+  vapidKeys.privateKey
+);
+
 // Use Routes
+app.post("/subscribe", (req, res) => {
+  //Get pushSubscription object
+  const subscription = req.body;
+
+  //Send 201-resource created
+  res.status(201).json({});
+
+  //Create payload
+  const payload = JSON.stringify({ title: "Push notification test" });
+
+  //Pass object into sendNotification
+  webPush
+    .sendNotification(subscription, payload)
+    .catch((err) => console.error(err));
+});
 app.use("/api/users", newUsers);
 app.use("/api/users/login", loginUser);
 app.use("/api/users/:userId/goal", Goals);
